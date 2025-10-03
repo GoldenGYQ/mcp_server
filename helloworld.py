bf9ad7004f215455e8ca401af8bac605e638df15
GoldenGYQ <1642826667@qq.com>
2025-10-03 21:03:06 +0800

    some thing useless

--- helloworld.py
+++ helloworld.py
@@ -5,7 +5,7 @@ mcp = FastMCP("HelloWorld")
 @mcp.tool()
 def helloworld() -> str:
     """hello world"""
-    return "hello world for mcp from 吉林大学通信工程学院 525实验室 GuanYingqi 20251003 - Updated!"
+    return "hello world for mcp from 吉林大学通信工程学院 525实验室祝你国庆快乐！"
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
