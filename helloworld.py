7d2453835a95598a45ae1726443ddf1710093f31
GoldenGYQ <1642826667@qq.com>
2025-10-03 19:55:14 +0800

    feat: 更新helloworld.py的返回信息，修改日期为20251003

--- helloworld.py
+++ helloworld.py
@@ -5,7 +5,7 @@ mcp = FastMCP("HelloWorld")
 @mcp.tool()
 def helloworld() -> str:
     """hello world"""
-    return "hello world for mcp from 吉林大学通信工程学院 525实验室 GuanYingqi 20241225 - Updated!"
+    return "hello world for mcp from 吉林大学通信工程学院 525实验室 GuanYingqi 20251003 - Updated!"
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
